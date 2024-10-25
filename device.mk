b1bdd065c92ba5b5efea63f5f7ac8a8332814fbf
AmeChanRain <downloadbot007@gmail.com>
2024-10-25 09:43:41 +0530

    zorn: Use shim for missing symbol Shim for missing symbol "_ZN7android13FilterWrapper17getParamReflectorEv" referenced by "/vendor/lib64/libcodec2_hidl@1.0.so"
    Signed-off-by: subhagamer <lordkazuya1@gmail.com>

--- device.mk
+++ device.mk
@@ -149,8 +149,8 @@ PRODUCT_PACKAGES += \
 
 #Media
 PRODUCT_PACKAGES += \
-    libcodec2_hidl_shim
-
+    libcodec2_hidl_shim \
+    libshim
 # Network
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
