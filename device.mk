3f2111b2296748b15ff6203ce33abe8fc1b71199
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-20 22:24:44 +0530

    onyx: Build IR example service
    Change-Id: Ibad1ae40780f1c6f59251409640fd8818f636510

--- device.mk
+++ device.mk
@@ -107,6 +107,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 # IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir-service.example
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.consumerir.xml
 
