15f2f07d235e54588b47a184aa6dd6d06687fea7
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-20 22:28:48 +0530

    onyx: Build sensors multihal service
    Change-Id: I9c3d5e65234e4dc3daac66c75996e7b8319950cb

--- device.mk
+++ device.mk
@@ -197,6 +197,9 @@ PRODUCT_PACKAGES += \
 
 
 # Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors-service.multihal
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.compass.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.compass.xml \
