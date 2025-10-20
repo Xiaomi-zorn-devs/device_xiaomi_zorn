435040f60a2baa5397b239c28f74cdcf1658da80
AdarshGrewal <adarshgrewal@gmail.com>
2025-10-20 22:29:20 +0530

    onyx: Build dynamic sensor hal
    Change-Id: I596656a3999fa789c535ee255d3fead5bf888268

--- device.mk
+++ device.mk
@@ -198,7 +198,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.multihal
+    android.hardware.sensors-service.multihal \
+    sensors.dynamic_sensor_hal
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
