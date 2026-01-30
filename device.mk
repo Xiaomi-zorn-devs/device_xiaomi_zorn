c79e271549b6fbee6773435e407405cd24321b2a
phhgsi <ajay24ce008@satiengg.in>
2026-01-30 07:34:38 +0530

    zorn: Enable QSPA Framework

--- device.mk
+++ device.mk
@@ -160,6 +160,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/properties/odm_GL.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_GL.prop \
     $(LOCAL_PATH)/properties/odm_IN.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_IN.prop
 
+# QSPA
+PRODUCT_PACKAGES += \
+    vendor.qti.qspa-service \
+    qspa_vendor.rc
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
